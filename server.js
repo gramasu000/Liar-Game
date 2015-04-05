5e9380f4f9137515798db21126cd796f871266d1
Allen Kim <akim003@citymail.cuny.edu>
2015-04-05 10:38:46 -0400

    Added a disconnect function

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Create a Jade object
+// Create a EJS object
 var ejs = require('ejs');
 // Create a Express JS server object
 var express = require('express');
@@ -27,22 +27,25 @@ var io = require('socket.io').listen(server);
 server.listen(3000);
 
 // Connection event
-io.sockets.on('connection', function (socket) { 
+io.sockets.on('connection', function (socket) {
 
 	// SetPseudo event
 	socket.on('setPseudo', function (data) {
     	socket.pseudo = data;
-      allpseudos.push(data);
-      socket.broadcast.emit('setPseudo', data);
+      	allpseudos.push(data);
+      	socket.broadcast.emit('setPseudo', data);
 
 	});
 
 	// Obtaining a sent message event
 	socket.on('message', function (message) {
-	
     	var data = { 'message' : message[0], 'pseudo' : socket.pseudo, 'recipient' : message[1]};
     	socket.broadcast.emit('message', data);
     	console.log("user " + socket.pseudo + " send this : " + message[0] + " to " + message[1]);
+	});
 
+	//Alerts when someone disconnects
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
 	});
 });
