04d1a44727005183acc6b80b88abefea11956f1b
Allen Kim <akim003@citymail.cuny.edu>
2015-05-03 20:28:23 -0400

    Added a How To Play button and state

--- public/game/Preloader.js
+++ public/game/Preloader.js
@@ -30,6 +30,7 @@ BasicGame.Preloader.prototype = {
 		this.load.image('titlepage', 'assets/exampleBackground.jpg');
 		this.load.image('hostGameButton', 'assets/hostGame.png');
 		this.load.image('joinGameButton', 'assets/joinGame.png');
+		this.load.image('howToPlayButton', 'assets/howToPlay.png')
 		this.load.image('roomButton', 'assets/roomButton.png');
 		this.load.image('backButton', 'assets/backButton.png');
 		this.load.audio('titleMusic', 'assets/mainMenu.mp3');
